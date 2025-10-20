e1ad34d5586ab6ca119d0fb886108cfbf256fd68
Roor Li <1563856124@qq.com>
2025-10-20 15:34:12 +0800

    inital commit

---
+++ src/main/java/com/opengms/yddback/YddbackApplication.java
@@ -0,0 +1,13 @@
+package com.opengms.yddback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YddbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YddbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/opengms/yddback/YddbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opengms.yddback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YddbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
